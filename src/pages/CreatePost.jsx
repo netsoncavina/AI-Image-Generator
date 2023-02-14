f733c97b42665c57d14a1d2401aae27983d63463
Netson Cavina <netsoncavina@gmail.com>
2023-02-14 16:02:07 -0300

    adicionados useStates

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -5,6 +5,15 @@ import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return <div>CreatePost</div>;
 };
 
