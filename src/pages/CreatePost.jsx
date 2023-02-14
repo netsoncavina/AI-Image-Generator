eef3ab9bfb7bf06223cbaf98994b608e6433964e
Netson Cavina <netsoncavina@gmail.com>
2023-02-14 16:09:00 -0300

    desenvolvimento pagina de criação de posts

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -14,7 +14,61 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  return <div>CreatePost</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      // navigate("/post/123");
+    }, 2000);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSurpriseMe = () => {
+    setGeneratingImg(true);
+    setTimeout(() => {
+      setGeneratingImg(false);
+      setForm((prev) => ({ ...prev, prompt: getRandomPrompt() }));
+    }, 2000);
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]"> Create </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w[500px]">
+          Create imaginative and visually stuning images through DALL-E AI and
+          share them with the community
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A Samurai riding a Horse on Mars, lomography."
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
